9a31e893ac1e9ad2c849d7a036cbbeafb87e5b26
quynIT <trungquyen2902@gmail.com>
2024-11-22 18:08:56 +0700

    delete-data

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import NotFound from '../page/NotFound.vue';
-import ProductImage from '@/components/ProductImage.vue';
-import AddImage from '../page/AddImage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import NotFound from "../page/NotFound.vue";
+import ProductImage from "@/components/ProductImage.vue";
+import AddImage from "../page/AddImage.vue";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: ProductImage,
   },
   {
-    path: '/add-image', 
-    name: 'AddImage',
+    path: "/add-image",
+    name: "AddImage",
     component: AddImage,
   },
   {
-    path: '/:pathMatch(.*)*', 
-    name: 'NotFound',
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
     component: NotFound,
-  }
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), 
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
-  
 export default router;
